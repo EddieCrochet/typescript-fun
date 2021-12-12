1f57fab981c19544b31b6d60943cece45e8a13a3
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-12-12 15:38:10 -0600

    added a separate counter component  to the application

--- src/UserInterface.ts
+++ src/UserInterface.ts
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 export default interface User{
     name: string;
     age: number;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import FirstComponent from './components/FirstComponent'
-import UserComponent from './components/UserComponent'
+import FirstComponent from './components/FirstComponent';
+import UserComponent from './components/UserComponent';
+import Counter from './components/Counter';
 ReactDOM.render(
     <div>
       <h1>Hello, Welcome to React and TypeScript</h1>
       <FirstComponent/>
+      <Counter/>
       <UserComponent name="John Doe" age={26} address="87 Summer St, Boston, MA 02110" dob={new Date()} />
     </div>,
     document.getElementById("root")
-);
+)
